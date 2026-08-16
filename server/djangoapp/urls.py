6fd56c23e9d5aa01541d0759498f00db16372ec1
G Savitha <savithasudha302005@gmail.com>
2026-08-16 20:54:37 +0530

    fix: map fetchDealer endpoint and update getdealerbyid output

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('get_dealers', views.get_dealerships, name='get_dealers'),
     path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
     path('dealer/<int:dealer_id>', views.get_dealer_details, name='get_dealer_details'),
+    path('fetchDealer/<int:dealer_id>', views.get_dealer_details, name='fetch_dealer_details'),
     
     # Reviews
     path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='get_dealer_reviews'),
